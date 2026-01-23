52950b63861ae42e2c11f2e0a3f33eec5bb87903
Moslm Shlaka <mike7olwo@gmail.com>
2026-01-23 14:18:52 +0300

    all categories modal added

--- src/components/HorizontalList.jsx
+++ src/components/HorizontalList.jsx
@@ -1,18 +1,62 @@
- 
-import '../styles/HorizontalList.css'; // Import the CSS file
+import '../styles/HorizontalList.css' // Import the CSS file
+import { useState } from 'react'
+import Button from 'react-bootstrap/Button'
+import Modal from 'react-bootstrap/Modal'
 
-const HorizontalList = ({items}) => {
+const HorizontalList = ({ items }) => {
+  const [showModal, setShowModal] = useState(false)
+  const handleClose = () => setShowModal(false)
+  const handleShow = () => setShowModal(true)
   // const items = ['Item 1', 'Item 2', 'Item 3', 'Item 4', 'Item 5', 'Item 6', 'Item 7', 'Item 8'];
 
   return (
-    <div className="scroll-container">
-      {items.map((item, index) => (
-        <div key={index} className="list-item">
-          {item}
+    <>
+      <div className='scroll-container mb-2'>
+        <div
+        onClick={handleShow}
+          className='list-item'
+          style={{
+            minWidth: '60px',
+            //  background: '#2a2d5a'
+          }}
+        >
+          All
         </div>
-      ))}
-    </div>
-  );
-};
+        {items.map((item, index) => (
+          <div key={index} className='list-item'>
+            {item}
+          </div>
+        ))}
+      </div> 
 
-export default HorizontalList;
+      <Modal
+        show={showModal}
+        onHide={handleClose}
+        size='lg'
+        // aria-labelledby='contained-modal-title-vcenter'
+        // centered
+        className="mt-5"
+       
+      >
+        <Modal.Header closeButton>
+          <Modal.Title>All Departments</Modal.Title>
+        </Modal.Header>
+        <Modal.Body  >
+          <div className="card" style={{height: '300px'}}>
+            <p>Woohoo, you are reading this text in a modal!</p>
+          </div>
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant='secondary' onClick={handleClose}>
+            Close
+          </Button>
+          <Button variant='primary' onClick={handleClose}>
+            Save Changes
+          </Button>
+        </Modal.Footer>
+      </Modal>
+    </>
+  )
+}
+
+export default HorizontalList
